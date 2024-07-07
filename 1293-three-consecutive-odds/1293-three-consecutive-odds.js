91450bcfbbcef794b13d64da41e5a6f419f68ff7
Rasul <112200142+jackflaggg@users.noreply.github.com>
2024-07-07 14:23:00 +0500

    Time: 50 ms (67.92%), Space: 49.2 MB (26.12%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.js
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.js
@@ -3,8 +3,8 @@
  * @return {boolean}
  */
 var threeConsecutiveOdds = function(arr) {
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] % 2 !== 0 && (arr[i+1] % 2 !== 0 && typeof arr[i+1] === 'number')  && (arr[i+2] % 2 !== 0  && typeof arr[i+2] === 'number'))  {
+	for (let i = 0; i < arr.length - 2; i++) {
+		if ((arr[i] * arr[i+1] * arr[i+2]) % 2 !== 0 )  {
 			return true
 		}
 	}
